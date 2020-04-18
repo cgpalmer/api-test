97a9386e8180a2d3cbf25a1f157a66fd4986ba45
cgpalmer <cgpalmer91@gmail.com>
2020-04-18 23:02:48 +0000

    adding in a test div

--- main.js
+++ main.js
@@ -74,6 +74,7 @@ $("#submit-button").on("click", function(event){
                 $("#card-container").append(pokemonCard);
                 }
             $("#card-container").css("display","block");
+            $("#testDiv").css("background-image", "url('https://images.pokemontcg.io/xy0/19_hires.png')");
             $("#loading").css("display", "none");
             $("#buttonContainer").css("display", "none");
             clearInterval(time);
@@ -82,75 +83,88 @@ $("#submit-button").on("click", function(event){
 
 
 
-//find out if an array has to be declared with a value.
-    //    $("#play").click(function(event){
-    //     var time = setInterval(frame, 1000);
-    //     event.preventDefault();
-    //     console.log(test);
-    //     function frame(){
-    //     if(test.length < 9){
-    //         var format = "We carefully choosing epic pokemon cards for you to see. Think of your favourite pokemon. Will you get to see it this time?";
-    //         //Change this to template literates
-    //         alert(format);
-    //     }else{
-    //     for (var j = 1; j<9; j++){ //then change this is an array can be declared empty.
-    //     var pokemonCard = $("<img class='img-card'>");
-    //     pokemonCard.attr("src", test[j]);
-    //     $("#card-container").append(pokemonCard);
-    //     }
-    //     clearInterval(time);
-    // }
-    //     }
-    //    });
+// //find out if an array has to be declared with a value.
+//     //    $("#play").click(function(event){
+//     //     var time = setInterval(frame, 1000);
+//     //     event.preventDefault();
+//     //     console.log(test);
+//     //     function frame(){
+//     //     if(test.length < 9){
+//     //         var format = "We carefully choosing epic pokemon cards for you to see. Think of your favourite pokemon. Will you get to see it this time?";
+//     //         //Change this to template literates
+//     //         alert(format);
+//     //     }else{
+//     //     for (var j = 1; j<9; j++){ //then change this is an array can be declared empty.
+//     //     var pokemonCard = $("<img class='img-card'>");
+//     //     pokemonCard.attr("src", test[j]);
+//     //     $("#card-container").append(pokemonCard);
+//     //     }
+//     //     clearInterval(time);
+//     // }
+//     //     }
+//     //    });
 
 
-// $("#play").on("click", function(event){
-//     event.preventDefault();
-   
-//     var test = ["test"];
-//      $.ajax({
-//         method:"GET",
-//         url: "https://cors-anywhere.herokuapp.com/https://api.pokemontcg.io/v1/cards?nationalPokedexNumber=85"
-//     }).then(function(response){
-//         console.log(response);
-//          const n = response.cards.length;
-        
-//         for (var i = 0; i<response.cards.length; i++){
-//             test.push(response.cards[i].imageUrlHiRes);
-//         }
-//         console.log(test);
-//         var pokemonCard = $("<img class='img-card'>");
-//         pokemonCard.attr("src", test[Math.floor((Math.random() * n))]);
-//         $("#card-container").append(pokemonCard);
-//     });
-// });
-
-
-
-
-
-
-// //tutorial https://www.youtube.com/watch?v=5zcSpVKxMao&t=1344s
-
-// //Build in some defensive design so that should you get an error response of 404 then it selects another one until it gets a correct one. 
-// //Build an array pusher to put each card into the same array. This can then have a do until it is 8 or do until it is 16. 
-
-// // $("#submit-button").on("click", function(event){
+// // $("#play").on("click", function(event){
 // //     event.preventDefault();
-// //     var pokemon = $("#search").val();
-// //     console.log(pokemon);
-
-// //     $.ajax({
+   
+// //     var test = ["test"];
+// //      $.ajax({
 // //         method:"GET",
-// //         //url: "https://cors-anywhere.herokuapp.com/https://api.pokemontcg.io/v1/cards?name="+pokemon
+// //         url: "https://cors-anywhere.herokuapp.com/https://api.pokemontcg.io/v1/cards?nationalPokedexNumber=85"
 // //     }).then(function(response){
 // //         console.log(response);
-
-//         //This little bit here can loop through the pokemon array and present the images at the end of the game. 
+// //          const n = response.cards.length;
+        
 // //         for (var i = 0; i<response.cards.length; i++){
-// //             var pokemonCard = $("<img class='img-card'>");
-// //             pokemonCard.attr("src", response.cards[i].imageUrlHiRes);
-// //             $("#card-container").append(pokemonCard);
+// //             test.push(response.cards[i].imageUrlHiRes);
 // //         }
+// //         console.log(test);
+// //         var pokemonCard = $("<img class='img-card'>");
+// //         pokemonCard.attr("src", test[Math.floor((Math.random() * n))]);
+// //         $("#card-container").append(pokemonCard);
 // //     });
-// });
+// // });
+
+
+
+
+
+
+// // //tutorial https://www.youtube.com/watch?v=5zcSpVKxMao&t=1344s
+
+// // //Build in some defensive design so that should you get an error response of 404 then it selects another one until it gets a correct one. 
+// // //Build an array pusher to put each card into the same array. This can then have a do until it is 8 or do until it is 16. 
+
+// // // $("#submit-button").on("click", function(event){
+// // //     event.preventDefault();
+// // //     var pokemon = $("#search").val();
+// // //     console.log(pokemon);
+
+// // //     $.ajax({
+// // //         method:"GET",
+// // //         //url: "https://cors-anywhere.herokuapp.com/https://api.pokemontcg.io/v1/cards?name="+pokemon
+// // //     }).then(function(response){
+// // //         console.log(response);
+
+// //         //This little bit here can loop through the pokemon array and present the images at the end of the game. 
+// // //         for (var i = 0; i<response.cards.length; i++){
+// // //             var pokemonCard = $("<img class='img-card'>");
+// // //             pokemonCard.attr("src", response.cards[i].imageUrlHiRes);
+// // //             $("#card-container").append(pokemonCard);
+// // //         }
+// // //     });
+// // });
+
+$("#testDiv").click(function(){
+    
+    $("#testDivCover").toggleClass("display");
+
+    $(this).toggleClass("display");
+});
+
+$("#testDivCover").click(function(){
+    $(this).toggleClass("display");
+   
+    $("#testDiv").toggleClass("display");
+});
